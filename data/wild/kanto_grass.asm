4ba826b13270a4feda1c7c79de41294e7baca147
zavytar <mickyjbarr@gmail.com>
2021-02-03 11:59:34 -0300

    changed wild mons to vanilla crystal

--- data/wild/kanto_grass.asm
+++ data/wild/kanto_grass.asm
@@ -5,84 +5,82 @@ KantoGrassWildMons:
 	map_id ROUTE_1
 	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
 	; morn
-	db 39, TANGELA
-	db 40, PONYTA
-	db 40, RAPIDASH
-	db 42, ARBOK
-	db 41, DODUO
-	db 43, DODRIO
-	db 43, DODRIO
+	db 2, PIDGEY
+	db 2, RATTATA
+	db 3, SENTRET
+	db 3, PIDGEY
+	db 6, FURRET
+	db 4, PIDGEY
+	db 4, PIDGEY
 	; day
-	db 39, TANGELA
-	db 40, PONYTA
-	db 40, RAPIDASH
-	db 42, ARBOK
-	db 41, DODUO
-	db 43, DODRIO
-	db 43, DODRIO
+	db 2, PIDGEY
+	db 2, RATTATA
+	db 3, SENTRET
+	db 3, PIDGEY
+	db 6, FURRET
+	db 4, PIDGEY
+	db 4, PIDGEY
 	; nite
-	db 39, TANGELA
-	db 40, POLIWHIRL
-	db 40, GOLBAT
-	db 40, POLIWHIRL
-	db 42, GOLBAT
-	db 42, GOLBAT
-	db 42, GOLBAT
+	db 2, HOOTHOOT
+	db 2, RATTATA
+	db 3, RATTATA
+	db 3, HOOTHOOT
+	db 6, RATICATE
+	db 4, HOOTHOOT
+	db 4, HOOTHOOT
 
 	map_id ROUTE_2
 	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
 	; morn
-	db 39, TANGELA
-	db 40, PONYTA
-	db 40, RAPIDASH
-	db 42, ARBOK
-	db 41, DODUO
-	db 43, DODRIO
-	db 43, DODRIO
+	db 3, CATERPIE
+	db 3, LEDYBA
+	db 5, PIDGEY
+	db 7, BUTTERFREE
+	db 7, LEDIAN
+	db 4, PIKACHU
+	db 4, PIKACHU
 	; day
-	db 39, TANGELA
-	db 40, PONYTA
-	db 40, RAPIDASH
-	db 42, ARBOK
-	db 41, DODUO
-	db 43, DODRIO
-	db 43, DODRIO
+	db 3, CATERPIE
+	db 3, PIDGEY
+	db 5, PIDGEY
+	db 7, BUTTERFREE
+	db 7, PIDGEOTTO
+	db 4, PIKACHU
+	db 4, PIKACHU
 	; nite
-	db 39, TANGELA
-	db 40, POLIWHIRL
-	db 40, GOLBAT
-	db 40, POLIWHIRL
-	db 42, GOLBAT
-	db 42, GOLBAT
-	db 42, GOLBAT
-
-	db -1 ; end
+	db 3, HOOTHOOT
+	db 3, SPINARAK
+	db 5, HOOTHOOT
+	db 7, NOCTOWL
+	db 7, ARIADOS
+	db 4, NOCTOWL
+	db 4, NOCTOWL
 
 	map_id ROUTE_22
 	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
 	; morn
-	db 39, TANGELA
-	db 40, PONYTA
-	db 40, RAPIDASH
-	db 42, ARBOK
-	db 41, DODUO
-	db 43, DODRIO
-	db 43, DODRIO
+	db 3, RATTATA
+	db 3, SPEAROW
+	db 5, SPEAROW
+	db 4, DODUO
+	db 6, PONYTA
+	db 7, FEAROW
+	db 7, FEAROW
 	; day
-	db 39, TANGELA
-	db 40, PONYTA
-	db 40, RAPIDASH
-	db 42, ARBOK
-	db 41, DODUO
-	db 43, DODRIO
-	db 43, DODRIO
+	db 3, RATTATA
+	db 3, SPEAROW
+	db 5, SPEAROW
+	db 4, DODUO
+	db 6, PONYTA
+	db 7, FEAROW
+	db 7, FEAROW
 	; nite
-	db 39, TANGELA
-	db 40, POLIWHIRL
-	db 40, GOLBAT
-	db 40, POLIWHIRL
-	db 42, GOLBAT
-	db 42, GOLBAT
-	db 42, GOLBAT
+	db 3, RATTATA
+	db 3, POLIWAG
+	db 5, RATTATA
+	db 4, POLIWAG
+	db 6, RATTATA
+	db 7, RATTATA
+	db 7, RATTATA
 
 	db -1 ; end
--- data/wild/kanto_water.asm
+++ data/wild/kanto_water.asm
@@ -2,6 +2,22 @@
 
 KantoWaterWildMons:
 
+	map_id PALLET_TOWN
+	db 6 percent ; encounter rate
+	db 35, TENTACOOL
+	db 30, TENTACOOL
+	db 35, TENTACRUEL
 
+	map_id VIRIDIAN_CITY
+	db 2 percent ; encounter rate
+	db 10, POLIWAG
+	db 5, POLIWAG
+	db 10, POLIWHIRL
+
+	map_id ROUTE_22
+	db 2 percent ; encounter rate
+	db 10, POLIWAG
+	db 5, POLIWAG
+	db 10, POLIWHIRL
 
 	db -1 ; end
