5d4a089d976f518d24802f8cae5c2cdd17dc4069
zavytar <mickyjbarr@gmail.com>
2021-02-03 20:04:25 -0300

    changed wild mons

--- data/wild/kanto_grass.asm
+++ data/wild/kanto_grass.asm
@@ -7,17 +7,17 @@ KantoGrassWildMons:
 	; morn
 	db 2, PIDGEY
 	db 2, RATTATA
-	db 3, SENTRET
+	db 3, RATTATA
 	db 3, PIDGEY
-	db 6, FURRET
+	db 6, RATTATA
 	db 4, PIDGEY
 	db 4, PIDGEY
 	; day
 	db 2, PIDGEY
 	db 2, RATTATA
-	db 3, SENTRET
+	db 3, RATTATA
 	db 3, PIDGEY
-	db 6, FURRET
+	db 6, RATTATA
 	db 4, PIDGEY
 	db 4, PIDGEY
 	; nite
@@ -25,7 +25,7 @@ KantoGrassWildMons:
 	db 2, RATTATA
 	db 3, RATTATA
 	db 3, HOOTHOOT
-	db 6, RATICATE
+	db 6, RATTATA
 	db 4, HOOTHOOT
 	db 4, HOOTHOOT
 
@@ -35,7 +35,7 @@ KantoGrassWildMons:
 	db 3, CATERPIE
 	db 3, LEDYBA
 	db 5, PIDGEY
-	db 7, BUTTERFREE
+	db 7, CATERPIE
 	db 7, LEDIAN
 	db 4, PIKACHU
 	db 4, PIKACHU
@@ -43,7 +43,7 @@ KantoGrassWildMons:
 	db 3, CATERPIE
 	db 3, PIDGEY
 	db 5, PIDGEY
-	db 7, BUTTERFREE
+	db 7, CATERPIE
 	db 7, PIDGEOTTO
 	db 4, PIKACHU
 	db 4, PIKACHU
@@ -52,7 +52,7 @@ KantoGrassWildMons:
 	db 3, SPINARAK
 	db 5, HOOTHOOT
 	db 7, NOCTOWL
-	db 7, ARIADOS
+	db 7, CATERPIE
 	db 4, NOCTOWL
 	db 4, NOCTOWL
 
@@ -82,5 +82,32 @@ KantoGrassWildMons:
 	db 6, RATTATA
 	db 7, RATTATA
 	db 7, RATTATA
+
+	map_id VIRIDIAN_FOREST
+	db 4 percent, 4 percent, 4 percent ; encounter rates: morn/day/nite
+	; morn
+	db 5, CATERPIE
+	db 5, WEEDLE
+	db 7, METAPOD
+	db 7, KAKUNA
+	db 5, PIDGEY
+	db 5, PIKACHU
+	db 9, PIDGEOTTO
+	; day
+	db 5, CATERPIE
+	db 5, WEEDLE
+	db 7, METAPOD
+	db 7, KAKUNA
+	db 5, PIDGEY
+	db 5, PIKACHU
+	db 9, PIDGEOTTO
+	; nite
+	db 5, CATERPIE
+	db 5, WEEDLE
+	db 7, METAPOD
+	db 7, KAKUNA
+	db 5, ODDISH
+	db 6, ODDISH
+	db 5, PIKACHU
 
 	db -1 ; end
