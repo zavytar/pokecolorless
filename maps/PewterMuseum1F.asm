87a959d51b751bd89f93202bd6d2bd284feb56e0
zavytar <mickyjbarr@gmail.com>
2021-02-04 18:34:09 -0300

    pewter museum scripts

--- maps/PewterMuseum1F.asm
+++ maps/PewterMuseum1F.asm
@@ -1,10 +1,95 @@
+	object_const_def ; object_event constants
+	const PEWTERMUSEUM1F_SCIENTIST1
+	const PEWTERMUSEUM1F_SCIENTIST2
+	const PEWTERMUSEUM1F_SCIENTIST3
+	const PEWTERMUSEUM1F_GRAMPS
+
 PewterMuseum1F_MapScripts:
 	db 0 ; scene scripts
 
 	db 0 ; callbacks
 
+PewterMuseum1F_Scientist1Script:
+	jumptextfaceplayer PewterMuseum1F_Scientist1Text
+
+PewterMuseum1F_Scientist2Script:
+	jumptextfaceplayer PewterMuseum1F_Scientist2Text
+
+PewterMuseum1F_Scientist3Script:
+	jumptextfaceplayer PewterMuseum1F_Scientist3Text
+
+PewterMuseum1F_GrampsScript:
+	jumptextfaceplayer PewterMuseum1F_GrampsText
+
+PewterMuseum1F_AerodactylFossil:
+	jumptextfaceplayer PewterMuseum1F_AerodactylFossilText
+
+PewterMuseum1F_KabutopsFossil:
+	jumptextfaceplayer PewterMuseum1F_KabutopsFossilText
+
+PewterMuseum1F_Scientist1Text:
+	text "Welcome to PEWTER"
+	line "MUSEUM!"
+
+	para "It's ¥50 for a"
+	line "child's ticket."
+
+	para "Would you like t-"
+
+	para "…"
+
+	para "Oh? Why, aren't"
+	line "you… OAK's grandson?"
+
+	para "Please, feel free"
+	line "to look around,"
+	cont "free of charge!"
+	done
+
+PewterMuseum1F_Scientist2Text:
+	text "I think that"
+	line "this chunk of"
+	cont "AMBER contains"
+	cont "#MON DNA."
+
+	para "It would be great"
+	line "if #MON could"
+	cont "be resurrected"
+	cont "from it…"
+
+	para "But, my colleagues"
+	line "just ignore me!"
+	done
+
+PewterMuseum1F_Scientist3Text:
+	text "There's a lab"
+	line "somewhere trying"
+	cont "to resurrect"
+	cont "ancient #MON"
+	cont "from AMBER."
+	done
+
+PewterMuseum1F_GrampsText:
+	text "That is one"
+	line "magnificent"
+	cont "fossil!"
+	done
+
+
+PewterMuseum1F_KabutopsFossilText:
+	text "It's a fossil of"
+	line "KABUTOPS."
+	done
+
+PewterMuseum1F_AerodactylFossilText:
+	text "It's a fossil of"
+	line "AERODACTYL."
+	done
+
+
 PewterMuseum1F_MapEvents:
 	db 0, 0 ; filler
+
 
 	db 3 ; warp events
 	warp_event 10,  7, PEWTER_CITY, 3
@@ -13,6 +98,12 @@ PewterMuseum1F_MapEvents:
 
 	db 0 ; coord events
 
-	db 0 ; bg events
+	db 2 ; bg events
+	bg_event  2,  6, BGEVENT_READ, PewterMuseum1F_KabutopsFossil
+	bg_event  2,  3, BGEVENT_READ, PewterMuseum1F_AerodactylFossil
 
-	db 0 ; object events
+	db 4 ; object events
+	object_event 12,  4, SPRITE_SCIENTIST, SPRITEMOVEDATA_STANDING_LEFT, 2, 2, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, PewterMuseum1F_Scientist1Script, -1
+	object_event 17,  2, SPRITE_SCIENTIST, SPRITEMOVEDATA_SPINRANDOM_FAST, 2, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, PewterMuseum1F_Scientist2Script, -1
+	object_event 12,  2, SPRITE_SCIENTIST, SPRITEMOVEDATA_STANDING_LEFT, 2, 2, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, PewterMuseum1F_Scientist3Script, -1
+	object_event  1,  4, SPRITE_GRAMPS, SPRITEMOVEDATA_STANDING_UP, 2, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, PewterMuseum1F_GrampsScript, -1
--- maps/PewterMuseum2F.asm
+++ maps/PewterMuseum2F.asm
@@ -1,7 +1,84 @@
+	object_const_def ; object_event constants
+	const PEWTERMUSEUM2F_GRAMPS
+	const PEWTERMUSEUM2F_SCIENTIST
+	const PEWTERMUSEUM2F_COOLTRAINERM
+	const PEWTERMUSEUM2F_POKEFAN
+	const PEWTERMUSEUM2F_TWIN
+
+
 PewterMuseum2F_MapScripts:
 	db 0 ; scene scripts
 
 	db 0 ; callbacks
+
+PewterMuseum2F_GrampsScript:
+	jumptextfaceplayer PewterMuseum2F_GrampsText
+
+PewterMuseum2F_ScientistScript:
+	jumptextfaceplayer PewterMuseum2F_ScientistText
+
+PewterMuseum2F_CooltrainerMScript:
+	jumptextfaceplayer PewterMuseum2F_CooltrainerMText
+
+PewterMuseum2F_PokefanScript:
+	jumptextfaceplayer PewterMuseum2F_PokefanText
+
+PewterMuseum2F_TwinScript:
+	jumptextfaceplayer PewterMuseum2F_TwinText
+
+PewterMuseum2F_SpaceShuttle:
+	jumptext PewterMuseum2F_SpaceShuttleText
+
+PewterMuseum2F_MoonStone:
+	jumptext PewterMuseum2F_MoonStoneText
+
+PewterMuseum2F_GrampsText:
+	text "July 20, 1969!"
+
+	para "The 1st lunar"
+	line "landing!"
+
+	para "I bought a color"
+	line "TV to watch it!"
+	done
+
+PewterMuseum2F_ScientistText:
+	text "We have a space"
+	line "exhibit now."
+	done
+
+PewterMuseum2F_CooltrainerMText:
+	text "MOON STONE?"
+
+	para "What's so special"
+	line "about it?"
+	done
+
+PewterMuseum2F_PokefanText:
+	text "Yeah, a PIKACHU"
+	line "soon, I promise!"
+	done
+
+PewterMuseum2F_TwinText:
+	text "I want a PIKACHU!"
+	line "It's so cute!"
+
+	para "I asked my Daddy"
+	line "to catch me one!"
+	done
+
+PewterMuseum2F_SpaceShuttleText:
+	text "SPACE SHUTTLE"
+	done
+
+PewterMuseum2F_MoonStoneText:
+	text "Meteorite that"
+	line "fell on MT.MOON."
+
+	para "Could it be a"
+	line "MOON STONE?"
+	done
+
 
 PewterMuseum2F_MapEvents:
 	db 0, 0 ; filler
@@ -12,5 +89,11 @@ PewterMuseum2F_MapEvents:
 	db 0 ; coord events
 
 	db 0 ; bg events
-
-	db 0 ; object events
+	bg_event 11,  2, BGEVENT_READ, PewterMuseum2F_SpaceShuttle
+	bg_event  2,  5, BGEVENT_READ, PewterMuseum2F_MoonStone
+	db 5 ; object events
+	object_event  4,  2, SPRITE_GRAMPS, SPRITEMOVEDATA_STANDING_UP, 2, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, PewterMuseum2F_GrampsScript, -1
+	object_event 12,  3, SPRITE_SCIENTIST, SPRITEMOVEDATA_STANDING_UP, 2, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, PewterMuseum2F_ScientistScript, -1
+	object_event  3,  6, SPRITE_COOLTRAINER_M, SPRITEMOVEDATA_STANDING_UP, 2, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, PewterMuseum2F_CooltrainerMScript, -1
+	object_event 11,  6, SPRITE_POKEFAN_M, SPRITEMOVEDATA_STANDING_UP, 2, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, PewterMuseum2F_PokefanScript, -1
+	object_event 12,  6, SPRITE_TWIN, SPRITEMOVEDATA_STANDING_UP, 2, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, PewterMuseum2F_TwinScript, -1
